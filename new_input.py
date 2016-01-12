b17e62011442da23dbfa0201cbbaf9d9a647a680
yusuk-e <yusuke.tanaka289@gmail.com>
2016-01-12 16:08:12 +0900

    test

--- new_input.py
+++ new_input.py
@@ -28,9 +28,10 @@ player_dic = {}
 #--data--
 elem = namedtuple("elem", "t, re_t, team, p, a, x, y, s")
 #絶対時刻, ハーフ相対時間，チームID，アクションID, x座標，y座標, success
-D = defaultdict(int)#アクションID付きボール位置データ D[counter]
+D = defaultdict(lambda: defaultdict(int))#アクションID付きボール位置データ D[counter]
 N = 0
-Seq_Team_of = defaultdict(lambda: defaultdict(int))
+Seq_Team1_of = defaultdict(int)
+Seq_Team2_of = defaultdict(int)
 N_Team1_of = -1
 N_Team2_of = -1
 
@@ -87,66 +88,67 @@ def input():
     A = tmp[0].split(".")
     B = A[0].split(":")
     C = A[1]
-    period1_start = int(B[0]) * 3600 + int(B[1]) * 60 + int(B[2])
+    period1_start = int(B[0]) * 3600 + int(B[1]) * 60 + int(B[2]) + int(C) * 10 ** -3
 
     temp = fin.readline().rstrip("\r\n").split(",")
     A = temp[0].split(".")
     B = A[0].split(":")
     C = A[1]
-    period1_end = int(B[0]) * 3600 + int(B[1]) * 60 + int(B[2])
-    pdb.set_trace()
+    period1_end = int(B[0]) * 3600 + int(B[1]) * 60 + int(B[2]) + int(C) * 10 ** -3
 
     temp = fin.readline().rstrip("\r\n").split(",")
     A = temp[0].split(".")
     B = A[0].split(":")
     C = A[1]
-    period2_start = int(B[0]) * 3600 + int(B[1]) * 60 + int(B[2])
+    period2_start = int(B[0]) * 3600 + int(B[1]) * 60 + int(B[2]) + int(C) * 10 ** -3
 
     temp = fin.readline().rstrip("\r\n").split(",")
     A = temp[0].split(".")
     B = A[0].split(":")
     C = A[1]
-    period2_end = int(B[0]) * 3600 + int(B[1]) * 60 + int(B[2])
+    period2_end = int(B[0]) * 3600 + int(B[1]) * 60 + int(B[2]) + int(C) * 10 ** -3
 
     temp = fin.readline().rstrip("\r\n").split(",")
     A = temp[0].split(".")
     B = A[0].split(":")
     C = A[1]
-    period3_start = int(B[0]) * 3600 + int(B[1]) * 60 + int(B[2])
+    period3_start = int(B[0]) * 3600 + int(B[1]) * 60 + int(B[2]) + int(C) * 10 ** -3
 
     temp = fin.readline().rstrip("\r\n").split(",")
     A = temp[0].split(".")
     B = A[0].split(":")
     C = A[1]
-    period3_end = int(B[0]) * 3600 + int(B[1]) * 60 + int(B[2])
+    period3_end = int(B[0]) * 3600 + int(B[1]) * 60 + int(B[2]) + int(C) * 10 ** -3
 
     temp = fin.readline().rstrip("\r\n").split(",")
     A = temp[0].split(".")
     B = A[0].split(":")
     C = A[1]
-    period4_start = int(B[0]) * 3600 + int(B[1]) * 60 + int(B[2])
+    period4_start = int(B[0]) * 3600 + int(B[1]) * 60 + int(B[2]) + int(C) * 10 ** -3
 
     temp = fin.readline().rstrip("\r\n").split(",")
     A = temp[0].split(".")
     B = A[0].split(":")
     C = A[1]
-    period4_end = int(B[0]) * 3600 + int(B[1]) * 60 + int(B[2])
+    period4_end = int(B[0]) * 3600 + int(B[1]) * 60 + int(B[2]) + int(C) * 10 ** -3
 
     counter = 0
     temp_player0_dic = {}
     temp_player1_dic = {}
+    flag = 0
+    time_fix = -1
     for row in fin:
         temp = row.rstrip("\r\n").split(",")
 
         A = temp[0].split(".")
         B = A[0].split(":")
         C = A[1]
-        t = int(B[0]) * 3600 + int(B[1]) * 60 + int(B[2])
+        t = int(B[0]) * 3600 + int(B[1]) * 60 + int(B[2]) + int(C) * 10 ** -3
 
         A = temp[1].split(".")
         B = A[0].split(":")
         C = A[1]
-        re_t = int(B[0]) * 3600 + int(B[1]) * 60 + int(B[2])
+        re_t = int(B[0]) * 3600 + int(B[1]) * 60 + int(B[2]) + int(C) * 10 ** -3
 
         team = int(temp[2])
         if team not in team_dic:
@@ -156,9 +158,11 @@ def input():
         if team_dic[team] == 0:
             if player not in temp_player0_dic:
                 temp_player0_dic[player] = len(temp_player0_dic)
+                player_dic[0] = temp_player0_dic
         elif team_dic[team] == 1:
             if player not in temp_player1_dic:
                 temp_player1_dic[player] = len(temp_player1_dic)
+                player_dic[1] = temp_player1_dic
         else:
             print "err"
 
@@ -180,15 +184,38 @@ def input():
 
         s = int(temp[7])
 
-        f = elem(t, re_t, team, player, action, x, y, s)
-        D[counter] = f
-        counter += 1
+        if time_fix != t:
+            if flag == 1:
+
+
+うまくいってない
+                if len(event) == 10:#コート上にいる人数が5,7,9,11人とか有り．とりあえず削除．
+                    for team_id in team_dic.itervalues():
+                        ids = np.where(event[:,2] == team_id)[0]
+                        #if len(ids) != 4 and len(ids) !=5:
+                        #print len(ids)
+                        #if len(ids) == 0:
+                        #    print 'err' #片方のチームのデータがないとき有り
+                        #pdb.set_trace()
+                        D[counter][team_id] = event[ids,:]
+                    #if np.shape(D[counter])[0] != 10:
+                    #    print 'error'
+                    #    pdb.set_trace() #10人いないとき有り
+                    counter += 1
+
+            time_fix = t
+            event = np.array([[t, re_t, team_dic[team], player_dic[team_dic[team]][player], action_dic[action], x, y, s]])
+
+        elif time_fix == t:
+            etemp = np.array([t, re_t, team_dic[team], player_dic[team_dic[team]][player], action_dic[action], x, y, s])
+            event = np.vstack([event,etemp])
+            flag = 1
 
     fin.close()
     N = counter - 1
-    player_dic[0] = temp_player0_dic
-    player_dic[1] = temp_player1_dic
 
+    for i in range(len(Stoppage)):#Stoppageもディクショナリの番号に直す
+        Stoppage[i] = action_dic[Stoppage[i]]
     Make_re_t()#各ピリオド開始からの相対時間を生成
     Reverse_Seq()#後半の攻撃を反転
     print "time:%f" % (time()-t0)
@@ -211,33 +238,133 @@ def Make_re_t():
 
     for n in range(N):
         x = D[n]
-        t = x.t
-        if period2_start < t and t < period2_end:
-            f = elem(t, x.re_t - period2_start, x.team, x.p, x.a, x.x, x.y, x.s)
-            D[n] = f
-        elif period3_start < t and t < period3_end:
-            f = elem(t, x.re_t - period3_start, x.team, x.p, x.a, x.x, x.y, x.s)
-            D[n] = f
-        elif period4_start < t and t < period4_end:
-            f = elem(t, x.re_t - period4_start, x.team, x.p, x.a, x.x, x.y, x.s)
-            D[n] = f
-
-        #注意：4ピリオド終了後にアクションがある
+        for team_id in team_dic.values():
+            x_team = x[team_id]
+            x_team_size = np.shape(x_team)[0]
+            if x_team_size == 7:
+                pdb.set_trace()
+            t = x_team[0][0]
+            for i in range(x_team_size):
+                if period2_start < t and t < period2_end:
+                    x_team[i][1] = t - period2_start
+                elif period3_start < t and t < period3_end:
+                    x_team[i][1] = t - period3_start
+                elif period4_start < t and t < period4_end:
+                    x_team[i][1] = t - period4_start
+            D[n][team_id] = x
+            #注意：4ピリオド終了後にアクションがある
 
 def Reverse_Seq():
 #--後半反転--
     for n in range(N):
         x = D[n]
-        t = x.t
+        x_size = np.shape(x)[0]
+        t = x[0][0]
         if t > period3_start:
-            f = elem(t, x.re_t, x.team, x.p, x.a, xmax - x.x, ymax - x.y, x.s)
-            D[n] = f
+            for i in range(x_size):
+                x[i][5] = xmax - x[i][5]
+                x[i][6] = ymax - x[i][6]
+            D[n] = x
 
+def Seq_Team_of():
+#--offense ボール軌跡--
+    global N_Team1_of, N_Team2_of
+    t0 = time()
+
+    n = 0
+    while n < N:
+        x = D[n]
+        pdb.set_trace()
+
+
+
+
+    pre_team = 0
+    flag = 0
+
+    while n < N:
+        team = D[n].team
+        action = D[n].a
+
+        if action in Stoppage:
+            flag = 1
+            n += 1
+
+        else:
+            if pre_team == team:
+                if team == 0:
+                    if flag == 1:
+                        N_Team1_of += 1
+                        flag = 0
+
+                    x = D[n]
+                    if np.size(Seq_Team1_of[N_Team1_of]) == 1: 
+                        #Seq_Team1_ofにまだデータがない場合
+                        f = np.array([x.t,x.re_t, x.team, x.p, x.a, x.x, x.y, x.s])
+                        Seq_Team1_of[N_Team1_of] = f
+                    else:
+                        f = np.array([x.t, x.re_t, x.team, x.p, x.a, x.x, x.y, x.s])
+                        Seq_Team1_of[N_Team1_of] = np.vstack([Seq_Team1_of[N_Team1_of],f])
+
+                    n += 1
+                    pre_team = team
+
+                elif team == 1:
+                    if flag == 1:
+                        N_Team2_of += 1
+                        flag = 0
+
+                    x = D[n]
+                    if np.size(Seq_Team2_of[N_Team2_of]) == 1: 
+                        #Seq_Team2_ofにまだデータがない場合
+                        f = np.array([x.t,x.re_t, x.team, x.p, x.a, x.x, x.y, x.s])
+                        Seq_Team2_of[N_Team2_of] = f
+                    else:
+                        f = np.array([x.t, x.re_t, x.team, x.p, x.a, x.x, x.y, x.s])
+                        Seq_Team2_of[N_Team2_of] = np.vstack([Seq_Team2_of[N_Team2_of],f])
+
+                    n += 1
+                    pre_team = team
+
+            else:
+                if team == 0:
+                    N_Team1_of += 1
+                    x = D[n]
+                    if np.size(Seq_Team1_of[N_Team1_of]) == 1: 
+                        #Seq_Team1_ofにまだデータがない場合
+                        f = np.array([x.t,x.re_t, x.team, x.p, x.a, x.x, x.y, x.s])
+                        Seq_Team1_of[N_Team1_of] = f
+                    else:
+                        f = np.array([x.t, x.re_t, x.team, x.p, x.a, x.x, x.y, x.s])
+                        Seq_Team1_of[N_Team1_of] = np.vstack([Seq_Team1_of[N_Team1_of],f])
+
+                    n += 1
+                    pre_team = team
+
+                elif team == 1:
+                    N_Team2_of += 1
+                    x = D[n]
+                    if np.size(Seq_Team2_of[N_Team2_of]) == 1: 
+                        #Seq_Team2_ofにまだデータがない場合
+                        f = np.array([x.t,x.re_t, x.team, x.p, x.a, x.x, x.y, x.s])
+                        Seq_Team2_of[N_Team2_of] = f
+                    else:
+                        f = np.array([x.t, x.re_t, x.team, x.p, x.a, x.x, x.y, x.s])
+                        Seq_Team2_of[N_Team2_of] = np.vstack([Seq_Team2_of[N_Team2_of],f])
+
+                    n += 1
+                    pre_team = team
+
+    N_Team1_of = len(Seq_Team1_of)
+    N_Team2_of = len(Seq_Team2_of)
 
 
 
 #--main--
 input()
 #データ読み込み
+
+Seq_Team_of()
+#オフェンス時のボール軌跡データ作成
 
 pdb.set_trace()
